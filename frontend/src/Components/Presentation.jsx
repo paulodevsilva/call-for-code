b2d1eb82607fc7182ef89eb45c1697cb38d4c685
Mayu <mayusatori@gmail.com>
2021-07-24 17:33:35 -0300

    Add presentation from home page

--- frontend/src/Components/Presentation.jsx
+++ frontend/src/Components/Presentation.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import img from './Imgs/imagePresentation.png';
+import retangle from './Imgs/retanglePresentation.png';
 
 class Presentation extends React.Component {
   render() {
     return (
-      <section>
-        
+      <section className="presentation-container">
+        <img src={ img } alt="Foto de um rio" className="presentation-img"/>
+        <img src={ retangle } alt="retângulo" className="retangle" />
+        <div className="presentation-text">
+          <h1 className="presentation-title">FRASE DE EFEITO</h1>
+          <p className="presentation-subtitle">TEXTO TEXTO TEXTO TEXTO</p>
+        </div>
+        <div className="closing-bar" />
       </section>
     );
   }
