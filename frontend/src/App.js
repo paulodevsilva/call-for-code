600701c8625db7577487fbda84bf3db6f8764977
paulodevsilva <silva.paulohr@gmail.com>
2021-07-25 17:36:44 -0300

    routes configs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,11 @@
 import './App.css';
-import Header from './Components/Header';
-import Hero from './Components/Hero';
-import AboutUs from './Components/AboutUs';
-import Benefits from './Components/Benefits';
-import JoinUs from './Components/JoinUs';
-import Team from './Components/Team';
-import Footer from './Components/Footer';
+
+import Routes from './routes';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Hero />
-      <AboutUs />
-      <Benefits />
-      <JoinUs />
-      <Team />
-      <Footer />
+      <Routes/>
     </div>
   );
 }
---
+++ frontend/src/Components/Form.jsx
@@ -0,0 +1,64 @@
+import { useState } from 'react';
+
+import api from  '../services/api';
+
+function Form({ history }) {
+  const [email, setEmail ] = useState('');
+  const [password, setPassword ] = useState('');
+  const [name, setName ] = useState('');
+
+
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    const user = await api.post('/api/users', { name, email, password, perfil: '2'});
+
+    console.log({user});
+
+    history.push('/')
+  }
+
+  return (
+    <>
+       <form onSubmit={handleSubmit}>
+          <label htmlFor="name">Nome *</label>
+          <input
+           type="text"
+           id="name"
+           placeholder="Digite seu nome"
+           value={name}
+           onChange={event => setName(event.target.value)}
+           />
+           
+           <label htmlFor="email">Email *</label>
+          <input
+           type="email"
+           id="email"
+           placeholder="Digite seu email" 
+           value={email}
+           onChange={event => setEmail(event.target.value)}
+
+           />
+
+           <label htmlFor="password">Senha *</label>
+          <input
+           type="password"
+           id="password"
+           placeholder="Digite sua senha"
+           value={password}
+           onChange={event => setPassword(event.target.value)}
+ 
+           />
+
+           <button className="btn" type="submit">Fazendas</button>
+           <button className="btn" type="submit">Parceiros</button>
+
+           
+      </form>
+    
+    </>
+  );
+}
+
+export default Form;
